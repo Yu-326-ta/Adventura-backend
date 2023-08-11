4c19f29a90f41068da1fe8d631af8cd4093cf9e3
Yu-326-ta <yoshinaga.yuta326@icloud.com>
2023-08-11 22:32:40 +0900

    implement ripository interface

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -1,9 +1,36 @@
 package repository
 
-import "echo-todo-api/model"
+import (
+	"echo-todo-api/model"
+
+	"gorm.io/gorm"
+)
 
 // interfaceとはmethodの一覧
 type IUserRepository interface {
 	GetUserByEmail(user *model.User, email string) error
 	CreateUser(user *model.User) error
+}
+
+type userRipository struct {
+	db *gorm.DB
+}
+
+func NewUserRipository(db *gorm.DB) IUserRepository {
+	return &userRipository{db}
+}
+
+func (ur *userRipository) GetUserByEmail(user *model.User, email string) error {
+	// 検索したユーザーが存在する場合、ポインタの指し示す先の値を検索したユーザーに変更
+	if err := ur.db.Where("email=?", email).First(user).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ur *userRipository) CreateUser(user *model.User) error {
+	if err := ur.db.Create(user).Error; err != nil {
+		return err
+	}
+	return nil
 }
