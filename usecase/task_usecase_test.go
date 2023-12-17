cd5253e5732440ad99ce9d31e76d3e43217e430d
Yu-326-ta <yoshinaga.yuta326@icloud.com>
2023-12-17 23:12:00 +0900

    fix test for not passing

--- usecase/task_usecase_test.go
+++ usecase/task_usecase_test.go
@@ -32,7 +32,7 @@ func Test_taskUsecase_GetAllTasks(t *testing.T) {
 		t.Errorf("error! %v", err)
 	}
 	for i, v := range tasks {
-		if v != expected[i] {
+		if v != expected[i+1] {
 			t.Errorf("GetAllTasks() = %v want %v", res, expected)
 		}
 	}
