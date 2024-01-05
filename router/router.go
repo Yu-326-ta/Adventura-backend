fe2833fd628d71e584b1e0a34078b4bfd93e26ec
Yu-326-ta <yoshinaga.yuta326@icloud.com>
2024-01-05 12:30:22 +0900

    change health version

--- router/router.go
+++ router/router.go
@@ -36,7 +36,7 @@ func NerRouter(uc controller.IUserController, tc controller.ITaskController) *ec
 	}))
 	healthResponse := map[string]string{
 		"status": "ok",
-		"version": "1",
+		"version": "2",
 	}
 	e.GET("/health", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, healthResponse)
